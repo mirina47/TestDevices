236ec83edf04c81fb290facf641a272644921f2c
mirina47 <115398948+mirina47@users.noreply.github.com>
2024-10-26 23:35:29 +0800

    Update Navbar.vue

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,24 +1,73 @@
 <template>
     <div id="app">
-      <nav>
-        <router-link class="item" to="/listDevices">Устройства</router-link>
+      <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #7AB2FF;">
+        <div class="container-fluid p-2">
+          <ul class="navbar-nav me-auto mb-8 mb-lg-0 h5 nav-centered">
+            <li class="nav-item"><router-link class="nav-link text" to="/listDevices">Устройства</router-link></li> 
+          </ul>
+        </div>
       </nav>
     </div>
   </template>
   
   <script>
       export default {
-          name: "NavBar",
-          data() {
-              return {
+        name: "NavBar",
+        data() {
+          return {};
   
-              };
-          }
-      };
+        }
+  };
   </script>
   
   <style>
-    .item {
-      margin-right: 5px;
-    }
+  
+  a.text:active {color: #000000} /* активная/посещенная ссылка */
+  a.text:visited {color: #000000}
+  a.text:hover {color: #ffffff}  /* при наведении */
+  
+  a.text {
+    text-decoration: none;
+    color: #000000;
+  }
+  
+  a.login:active {color: #ffffff} /* активная/посещенная ссылка */
+  a.login:hover {color: #e1e1e1}  /* при наведении */
+  a.login {
+    text-decoration: none;
+    color: #ffffff;
+  }
+  
+  a.logout:active {color: #000000} /* активная/посещенная ссылка */
+  a.logout:hover {color: #bb1d1d}  /* при наведении */
+  a.logout {
+    text-decoration: none;
+    color: #000000;
+  }
+  
+  .navbar {
+    text-align: center;
+    margin-top: -20px;
+    margin-bottom: 20px;
+  }
+  
+  .navbar-nav > li{
+    padding-left:20px;
+    padding-right:20px;
+    font-weight: 600;
+  }
+  
+  .nav-centered {
+    width: 100%;
+    justify-content: center;
+  }
+  
+  .nav-a {
+    margin: 5px;
+  }
+  
+  .navbar ul{
+    flex-wrap: wrap;
+  }
+  
   </style>
